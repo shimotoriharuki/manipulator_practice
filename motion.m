2c95ea0de8e94de648420e1dcd7d801504cb8eec
Haruki <underbird.h@gmail.com>
2024-02-20 17:06:15 +0900

    サイン波

--- motion.m
+++ motion.m
@@ -10,10 +10,13 @@ robot = Manipulator_3DOF_2D([0; 0; 0], [1; 1]);
 % end
 
 %逆運動学
+dt = 0.1;
 theta = 0;
-for position = 2 : -0.1 : 1
-    theta = theta+0.6;
-    robot = robot.setPosition([position; 0; 0]);
+x = 2;
+for t = 0 : dt : 10
+    theta = theta + pi * dt;
+    x = x - 0.2 * dt;
+    robot = robot.setPosition([x; 0.5*sin(theta); 0]);
     robot = robot.setOrientation(0);
     robot = robot.calcInverseKinematics;
     robot = robot.updateRobotState;

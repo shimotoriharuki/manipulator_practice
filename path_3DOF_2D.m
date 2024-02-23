ca999dcc9c7f99e4f67c6ffde9ac0d6dcb660012
Haruki <underbird.h@gmail.com>
2024-02-23 18:47:36 +0900

    とりあえず線はつながった

--- path_3DOF_2D.m
+++ path_3DOF_2D.m
@@ -6,15 +6,34 @@ l = 1;%[m]
 
 syms v1(t) v2(t) v3(t)
 
-V_m = l / (T_f - t_acc);
+%速度
+V_m =l / (T_f - t_acc);
 
 v1(t) = (V_m / t_acc) * t;
-v2(t) = V_m;
-v3(t) = V_m - (V_m / t_acc) * (t - T_f + t_acc);
+v2(t) = v1(t_acc);
+v3(t) = v2(T_f - t_acc) - (V_m / t_acc) * (t - T_f + t_acc);
 
+subplot(2, 1, 1)
 fplot(t, v1, [0, t_acc]);
 hold on
 fplot(t, v2, [t_acc, T_f - t_acc]);
 fplot(t, v3, [T_f - t_acc, T_f]);
-
 hold off
+
+%位置
+x1 = int(v1);
+x2 = int(v2);
+x3 = int(v3);
+
+%微妙？？
+x2 = x2 - (x2(t_acc) - x1(t_acc));
+x3 = int(v3) - (x3(T_f - t_acc) - x2(T_f - t_acc));
+
+subplot(2, 1, 2)
+fplot(t, x1, [0, t_acc]);
+hold on
+fplot(t, x2, [t_acc, T_f - t_acc]);
+fplot(t, x3, [T_f - t_acc, T_f]);
+hold off
+% xlim([0, 5])
+% ylim([0, 1])

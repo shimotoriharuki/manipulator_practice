861241228bae31109acb7ccc3fe8a5ebc78030fa
Haruki <underbird.h@gmail.com>
2024-02-23 17:44:48 +0900

    台形加速プロット

--- path_3DOF_2D.m
+++ path_3DOF_2D.m
@@ -4,20 +4,17 @@ T_f = 5; %[s]
 t_acc = 1;%[s]
 l = 1;%[m]
 
-V_m = l/(T_f - t_acc);
+syms v1(t) v2(t) v3(t)
 
-t = 0 : dt : T_f;
-t01 = t(1 : find(t==t_acc));
-t12 = t(find(t==t_acc) : find(t==T_f - t_acc));
-t23 = t(find(t==T_f - t_acc) : end);
+V_m = l / (T_f - t_acc);
 
-v1 = (V_m / t_acc) * t;
-v2 = V_m;
-v3 = V_m - (V_m / t_acc) * (t - T_f + t_acc);
+v1(t) = (V_m / t_acc) * t;
+v2(t) = V_m;
+v3(t) = V_m - (V_m / t_acc) * (t - T_f + t_acc);
 
-plot(t, v1);
+fplot(t, v1, [0, t_acc]);
 hold on
-plot(t, v2);
-plot(t, v3);
+fplot(t, v2, [t_acc, T_f - t_acc]);
+fplot(t, v3, [T_f - t_acc, T_f]);
 
 hold off

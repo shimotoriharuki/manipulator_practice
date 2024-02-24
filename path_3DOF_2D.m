e3877dd0237d61a5bd7ef05aa6df289d280c592c
Haruki <underbird.h@gmail.com>
2024-02-24 17:46:27 +0900

    きれいにした

--- path_3DOF_2D.m
+++ path_3DOF_2D.m
@@ -1,25 +1,12 @@
 clear
-dt = 0.1;
-T_f = 5; %[s]
-t_acc = 1;%[s]
-l = 1;%[m]
 
-syms v1(t) v2(t) v3(t)
-
-%速度
-V_m =l / (T_f - t_acc);
+syms T_f t_acc l v1(t) v2(t) v3(t) V_m
 
 v1(t) = (V_m / t_acc) * t;
 v2(t) = v1(t_acc);
 v3(t) = v2(T_f - t_acc) - (V_m / t_acc) * (t - T_f + t_acc);
 
-
-subplot(2, 1, 1)
-fplot(t, v1, [0, t_acc]);
-hold on
-fplot(t, v2, [t_acc, T_f - t_acc]);
-fplot(t, v3, [T_f - t_acc, T_f]);
-hold off
+fv = piecewise((0 <= t) & (t <= t_acc), v1, (t_acc <= t) & (t <= T_f - t_acc), v2, (T_f - t_acc <= t) & (t <= T_f), v3); % まとめる
 
 %位置
 x1 = int(v1);
@@ -27,14 +14,37 @@ x2 = int(v2);
 x3 = int(v3);
 
 %積分定数
+x1 = x1 + 0;
 x2 = x2 + (x1(t_acc) - x2(t_acc));
 x3 = x3 + (x2(T_f - t_acc) - x3(T_f - t_acc));
 
+fx = piecewise((0 <= t) & (t <= t_acc), x1, (t_acc <= t) & (t <= T_f - t_acc), x2, (T_f - t_acc <= t) & (t <= T_f), x3); % まとめる
+
+%tex形式で出力
+fv_latex = latex(fv);
+fx_latex = latex(fx);
+
+%最大速度
+% V_m = l / (T_f - t_acc);
+fx = subs(fx, V_m, l / (T_f - t_acc));
+fv = subs(fv, V_m, l / (T_f - t_acc));
+
+%数値を代入してプロット
+dt = 0.1;
+T_f_ = 5; %[s]
+t_acc_ = 1;%[s]
+l_ = 1;%[m]
+
+fv = subs(fv, T_f, T_f_);
+fv = subs(fv, t_acc, t_acc_);
+fv = subs(fv, l, l_);
+
+fx = subs(fx, T_f, T_f_);
+fx = subs(fx, t_acc, t_acc_);
+fx = subs(fx, l, l_);
+
+subplot(2, 1, 1)
+fplot(t, fv);
+
 subplot(2, 1, 2)
-fplot(t, x1, [0, t_acc]);
-hold on
-fplot(t, x2, [t_acc, T_f - t_acc]);
-fplot(t, x3, [T_f - t_acc, T_f]);
-hold off
-% xlim([0, 5])
-% ylim([0, 1])
+fplot(t, fx);

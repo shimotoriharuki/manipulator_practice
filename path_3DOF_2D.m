f4aeeb9d735a6e08d67f7c5bdca0fe750b3311f0
Haruki <underbird.h@gmail.com>
2024-02-27 14:33:02 +0900

    直線OK

--- path_3DOF_2D.m
+++ path_3DOF_2D.m
@@ -33,19 +33,23 @@ T_f_ = 1; %[s]
 t_acc_ = 0.2;%[s]
 
 tt = 0 : dt : T_f_;
-x_0 = [0; 0]; %初期位置
-x_f = [tt; sin((pi/2) * tt)]; %目標位置
-l_ = [x_f(1, end) - x_0(1, 1); x_f(2, end) - x_0(2, 1)];%[m] %初期位置から目標位置までの軌道の道のりの距離
+x_0 = [0; 1]; %初期位置
+x_f = [1; 0]; %目標位置
+% x_f = [tt; sin((pi/2) * tt)]; %目標位置
+l_ = [norm(x_f - x_0)];%[m] %初期位置から目標位置までの軌道の道のりの距離
+% l_ = [x_f(1, :) - x_0(1, :); x_f(2, :) - x_0(2, :)];%[m] %初期位置から目標位置までの軌道の道のりの距離
 
 fv = subs(fv, T_f, T_f_);
 fv = subs(fv, t_acc, t_acc_);
-fv_x = subs(fv, l, l_(1)); %x成分
-fv_y = subs(fv, l, l_(2)); %y成分
+fv = subs(fv, l, l_);
+% fv_x = subs(fv, l, l_(1)); %x成分
+% fv_y = subs(fv, l, l_(2)); %y成分
 
 fx = subs(fx, T_f, T_f_);
 fx = subs(fx, t_acc, t_acc_);
-fx_x = subs(fx, l, l_(1)); %x成分
-fx_y = subs(fx, l, l_(2)); %y成分
+fx = subs(fx, l, l_);
+% fx_x = subs(fx, l, l_(1)); %x成分
+% fx_y = subs(fx, l, l_(2)); %y成分
 
 %プロット
 m = 4; %プロットの行
@@ -58,35 +62,37 @@ xlabel("x [m]")
 ylabel("y [m]")
 
 subplot(m, n, 3)
-fplot(t, fv_x);
+fplot(t, fv);
 title("vx-tグラフ")
 xlabel("t [s]")
 ylabel("vx [m/s]")
 
-subplot(m, n, 4)
-fplot(t, fv_y);
-title("vy-tグラフ")
-xlabel("t [s]")
-ylabel("vy [m/s]")
+% subplot(m, n, 4)
+% fplot(t, fv);
+% title("vy-tグラフ")
+% xlabel("t [s]")
+% ylabel("vy [m/s]")
 
 subplot(m, n, 5)
-fplot(t, fx_x);
+fplot(t, fx);
 title("x-tグラフ")
 xlabel("t [s]")
 ylabel("x [m]")
 
-subplot(m, n, 6)
-fplot(t, fx_y);
-title("y-tグラフ")
-xlabel("t [s]")
-ylabel("y [m]")
+% subplot(m, n, 6)
+% fplot(t, fx);
+% title("y-tグラフ")
+% xlabel("t [s]")
+% ylabel("y [m]")
 
 subplot(m, n, [7, 8])
 % tt = 0 : dt : T_f_;
-path_x = x_0(1, :) + (x_f(1, end) - x_0(1, :) / l_(1, :)) .* fx_x(tt);
-path_y = x_0(2, :) + (x_f(2, :) - x_0(2, :) / l_(2, :)) .* fx_y(tt);
+d_path = ((x_f - x_0) / l_) * fv(tt) * dt;
+path = x_0 + [cumsum(d_path(1, :)); cumsum(d_path(2, :))];
+% path_x = x_0(1, :) + (x_f(1, end) - x_0(1, :) / l_(1, :)) .* fx_x(tt);
+% path_y = x_0(2, :) + (x_f(2, :) - x_0(2, :) / l_(2, :)) .* fx_y(tt);
 
-scatter(path_x, path_y);
+scatter(path(1, :), path(2, :));
 title("手先位置")
 xlabel("x [m]")
 ylabel("y [m]")

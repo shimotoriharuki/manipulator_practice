171c46d3e48c8ae549ccbb838964235e89cefe7e
Haruki <underbird.h@gmail.com>
2024-03-01 15:31:04 +0900

    逆運動学OK

--- Manipulator_3DOF_3D.m
+++ Manipulator_3DOF_3D.m
@@ -96,20 +96,21 @@ classdef Manipulator_3DOF_3D
             l3 = obj.arm_links_(4);
             l4 = obj.arm_links_(5);
             %P3の位置
-            x3 = obj.hand_position_(1);
-            y3 = obj.hand_position_(2);
-            z3 = obj.hand_position_(3) + l4;
+            x3_shift = obj.hand_position_(1);
+            y3_shift = obj.hand_position_(2);
+            z3_shift = obj.hand_position_(3) + l4 - l0 - l1; %P1を原点に持ってくるように加工
 
             %各関節角度を計算
-            L13 = sqrt(x3^2 + y3^2 + z3^2); %P1からP3までの距離
+            L13 = sqrt(x3_shift^2 + y3_shift^2 + z3_shift^2); %P1からP3までの距離
             th2 = pi - acos((-L13^2 + l2^2 + l3^2) / (2 * l2 * l3));
 
-            l13 = sqrt(x3^2 + y3^2); %L13を斜辺としたときの直角三角形の底辺
-            th1 = atan2(z3 - (l0 + l1), l13) - acos((-l3^2 + L13^2 + l2^2) / (2 * L13 * l2));
+            l13 = sqrt(x3_shift^2 + y3_shift^2); %L13を斜辺としたときの直角三角形の底辺
+            th1 = atan2(z3_shift, l13) - acos((-l3^2 + L13^2 + l2^2) / (2 * L13 * l2));
 
-            th0 = atan2(y3, x3);
+            th0 = atan2(y3_shift, x3_shift);
             
             obj.joint_angles_ = [th0; th1; th2];
+            
         end
 
         function obj = calcInverseKinematicsUsingJacobian(obj, d_P, d_th)
